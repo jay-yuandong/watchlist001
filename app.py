cbe0b7390161b01ff1f090b7296d3122581d62dd
yuandong <296363882@qq.com>
2020-12-02 17:01:01 +0800

    add index page

--- app.py
+++ app.py
@@ -1,9 +1,22 @@
-from flask import Flask,escape,url_for
+from flask import Flask,escape,url_for,render_template
 app=Flask(__name__)
+name = 'Grey Li'
+movies = [
+    {'title': 'My Neighbor Totoro', 'year': '1988'},
+    {'title': 'Dead Poets Society', 'year': '1989'},
+    {'title': 'A Perfect World', 'year': '1993'},
+    {'title': 'Leon', 'year': '1994'},
+    {'title': 'Mahjong', 'year': '1996'},
+    {'title': 'Swallowtail Butterfly', 'year': '1996'},
+    {'title': 'King of Comedy', 'year': '1999'},
+    {'title': 'Devils on the Doorstep', 'year': '1999'},
+    {'title': 'WALL-E', 'year': '2008'},
+    {'title': 'The Pork of Music', 'year': '2012'},
+]
 #装饰器为这个函数绑定url，访问时触发这个函数的返回值
 @app.route('/')
-def hello():
-    return 'hello'
+def index():
+    return render_template('index.html',name=name,movies=movies)
 
 @app.route('/user/<name>')
 def user_page(name):
@@ -16,3 +29,4 @@ def test_url_for():
     print(url_for('test_url_for'))
     print(url_for('test_url_for',num=2))
     return 'test page'
+
