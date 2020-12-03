f1625bc6173671165e797d411173f3c937c80f19
yuandong <296363882@qq.com>
2020-12-03 14:36:58 +0800

    Add database support with Flask-SQLAlchemy

--- app.py
+++ app.py
@@ -1,22 +1,62 @@
-from flask import Flask,escape,url_for,render_template
-
-name = 'Grey Li'
-movies = [
-    {'title': 'My Neighbor Totoro', 'year': '1988'},
-    {'title': 'Dead Poets Society', 'year': '1989'},
-    {'title': 'A Perfect World', 'year': '1993'},
-    {'title': 'Leon', 'year': '1994'},
-    {'title': 'Mahjong', 'year': '1996'},
-    {'title': 'Swallowtail Butterfly', 'year': '1996'},
-    {'title': 'King of Comedy', 'year': '1999'},
-    {'title': 'Devils on the Doorstep', 'year': '1999'},
-    {'title': 'WALL-E', 'year': '2008'},
-    {'title': 'The Pork of Music', 'year': '2012'},
-]
-#装饰器为这个函数绑定url，访问时触发这个函数的返回值
+from flask import Flask,render_template
+from flask_sqlalchemy import SQLAlchemy
+import os,sys,click
 app=Flask(__name__)
+#判断当前操作系统
+win=sys.platform.startswith('win')
+if win: #是windows就使用三个斜线
+    prefix='sqlite:///'
+else:
+    prefix='sqlite:////'
+#配置数据库的地址
+app.config['SQLALCHEMY_DATABASE_URI']=prefix+os.path.join(app.root_path,'data.db')
+#关闭对模型修改的监控
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
+#装饰器为这个函数绑定url，访问时触发这个函数的返回值
+
+#初始化扩展，传入程序实例app
+db=SQLAlchemy(app)
+
+#创建user表
+class User(db.Model):
+    id=db.Column(db.Integer,primary_key=True)
+    name=db.Column(db.String(20))
+# 创建movie表
+class Movie(db.Model):
+    id=db.Column(db.Integer,primary_key=True)
+    title=db.Column(db.String(60))
+    year=db.Column(db.String(4))
+#添加命令行
+@app.cli.command()
+def forge():
+    """Generate fake data. """
+    db.create_all()
+    name = 'Grey Li'
+    movies = [
+        {'title': 'My Neighbor Totoro', 'year': '1988'},
+        {'title': 'Dead Poets Society', 'year': '1989'},
+        {'title': 'A Perfect World', 'year': '1993'},
+        {'title': 'Leon', 'year': '1994'},
+        {'title': 'Mahjong', 'year': '1996'},
+        {'title': 'Swallowtail Butterfly', 'year': '1996'},
+        {'title': 'King of Comedy', 'year': '1999'},
+        {'title': 'Devils on the Doorstep', 'year': '1999'},
+        {'title': 'WALL-E', 'year': '2008'},
+        {'title': 'The Pork of Music', 'year': '2012'},
+    ]
+    user=User(name=name)
+    db.session.add(user)
+    for m in movies:
+        movie=Movie(title=m['title'],year=m['year'])
+        db.session.add(movie)
+    db.session.commit()
+    click.echo('done.')
+
+
 @app.route('/')
 def index():
-    return render_template('index.html',name=name,movies=movies)
+    user=User.query.first()
+    movies=Movie.query.all()
+    return render_template('index,html',user=user,movie=movies)
 
 
